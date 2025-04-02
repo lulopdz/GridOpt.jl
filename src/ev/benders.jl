c66f787a91aa0cb4d91f0f2f5913ce80385e5c41
lulopdz <lulopdz@gmail.com>
2025-04-02 10:37:48 -0400

    Benders new version

--- src/ev/benders.jl
+++ src/ev/benders.jl
@@ -1,133 +1,194 @@
+# This code is the benders decomposition for a generation expansion problem 
+# including EV aggregator that has V2G capabilities. 
+
+# Authors: Luis Lopez and Andrew Moffat 
+
+# ==============================================================================
+# Importing packages
 using JuMP
-using GLPK
+using Ipopt, GLPK
+using Plots, LaTeXStrings
 
-const SCENARIOS = [
-    (demand = 290.0, weight = 3000.0),
-    (demand = 550.0, weight = 2760.0),
-    (demand = 400.0, weight = 3000.0),
+# Importing functions
+include("utils.jl")
+include("models.jl")
+
+# ==============================================================================
+# Parse MATPOWER data from file
+mpc = parse_mpc("GridOpt.jl/data/case5_strg.m")
+
+# Scenerio-specific parameters
+scenarios = [
+    (df = 1.3, weight = 3000.0),
+    (df = 1.2, weight = 2760.0),
+    (df = 1.4, weight = 3000.0),
 ]
-costE = 35.0
-costC = 25.0
-const CAP_GEN = 400.0
-const CAP_MAX = 500.0
-const BLOCKSIZE = 10.0
-const cost_cap = 70000.0
+
+# Sets and parameters
+Ω = collect(1:length(scenarios))
+
+# Existing generation and demand
+nG = length(mpc["gen"]) 
+Pgmax = [mpc["gen"][i][9] for i in 1:nG]
+Pgmax_total = sum(Pgmax)
+PDtotal = sum(mpc["bus"][i][3] for i in 1:length(mpc["bus"]))
+
+# Parameters new generation (now for multiple candidates)
+costC_op = [30.0, 20.0]
+Pcmax = [500.0, 1000.0]
+PcOpt = [20.0, 20.0]
+costC_inv = [70000.0, 75000.0]
+busC = [2, 4]
+
+nC = length(costC_op)
+C = collect(1:nC)
 
 # ==============================================================================
-# Subproblem definition
-# 
-# Given pCmax (the capacity), return:
-#   - The subproblem cost
-#   - The dual for "y <= pCmax"
-#   - Feasibility status
-# 
-# We solve:
-#      min  costE*x + costC*y
-#      s.t. x + y = demand
-#           0 <= x <= CAP_GEN
-#           0 <= y <= pCmax
-# 
-# ==============================================================================
-function solve_subproblem(demand, pCmax; 
-                          costE=costE, costC=costC,
-                          solver=GLPK.Optimizer)
-
-    # Quick check for infeasibility:
-    if demand > CAP_GEN + pCmax
-        return (0.0, 0.0, false)
-    end
-
-    model = Model(solver)
-    @variable(model, 0 <= x <= CAP_GEN)
-    @variable(model, 0 <= y)
-    @constraint(model, yup, y <= pCmax)
-    @constraint(model, x + y == demand)
-    @objective(model, Min, costE*x + costC*y)
-
-    optimize!(model)
-
-    if termination_status(model) != MOI.OPTIMAL
-        return (0.0, 0.0, false)
-    end
-
-    subcost = objective_value(model)
-    # Dual for y <= pCmax:
-    muY = dual(yup)  
-    return (subcost, muY, true)
-end
-
-# ---------------------------
 # Master model
 # 
-#  min sum(weight[o]*θ[o]) + cost_cap*pC
-#  s.t. 0 <= pC <= CAP_MAX
+#  min sum(weight[o]*θ[o]) + sum_i(cost_cap[i]*pC[i])
+#  s.t. 0 <= pC[i] <= CAP_MAX[i]
 #       θ[o] >= 0
-#   plus Benders cuts that tie θ[o] to subproblem cost
-# ---------------------------
+#   plus Benders cuts that tie θ[o] to subproblem cost components
+
+# ==============================================================================
 master = Model(GLPK.Optimizer)
 
-# ===========================
-# continuous pC in [0, CAP_MAX]
-# @variable(master, 0 <= pC <= CAP_MAX)
-# @variable(master, θ[1:length(SCENARIOS)] >= 0)
+# Define integer decision for each candidate
+@variable(master, z[c in C], Int)
+@expression(master, pC[c in C], PcOpt[c]*z[c])
+@constraint(master, [c in C], 0 <= pC[c] <= Pcmax[c])
+@expression(master, total_pC, sum(pC[c] for c in C))
 
-# @objective(master, Min, 
-#     sum(SCENARIOS[i].weight * θ[i] for i in eachindex(SCENARIOS)) + cost_cap * pC
-# )
+# Scenario-specific cost variables
+@variable(master, θ[ω in Ω] >= 0)
 
-# ===========================
-# Integer 
-
-@variable(master, 0 <= z <= CAP_MAX/BLOCKSIZE, Int)
-@expression(master, pC, BLOCKSIZE * z)
-@variable(master, θ[1:length(SCENARIOS)] >= 0)
+# Master objective: scenario cost plus investment costs for the new candidate(s)
 @objective(master, Min, 
-    sum(SCENARIOS[i].weight * θ[i] for i in eachindex(SCENARIOS)) + cost_cap * pC
+    sum(scenarios[ω].weight * θ[ω] for ω in Ω) +
+    sum(costC_inv[c] * pC[c] for c in C)
 )
 
 max_iters = 10
+
+his = Dict(
+    :pC => Vector{Vector{Float64}}(),
+    :master_obj => Vector{Float64}(),
+    :muY => Vector{Vector{Float64}}(),
+    :subcost => Vector{Float64}()
+)
+
+tolerance = 1e-3
+master_obj_k = Inf
+
 for k in 1:max_iters
+    global master_obj_k
     println("\nBenders Iteration $k")
     optimize!(master)
-    pC_val = value(pC)
-    println("  Master pC = $pC_val")
-    println("  Master Obj = ", objective_value(master))
+    
+    # Retrieve current candidate capacities as a vector
+    pC_val = [value(pC[c]) for c in C]
+    master_obj = objective_value(master)
+    println("  Master candidate capacities pC = ", pC_val)
+    println("  Master Obj = ", master_obj)
+    push!(his[:pC], pC_val)
+    push!(his[:master_obj], master_obj)
 
+    
     # For each scenario, solve subproblem
-    for i in eachindex(SCENARIOS)
-        sc = SCENARIOS[i]
-        (subcost, muY, feasible) = solve_subproblem(sc.demand, pC_val)
-
+    for ω in Ω
+        sc = scenarios[ω]
+        # Pass candidate capacities, candidate costs and bus info to the model
+        (subcost, muY, feasible, maxD) = market_model(mpc, sc.df, pC_val, costC_op, busC)
+        
+        push!(his[:muY], muY)
+        push!(his[:subcost], subcost)
+        
         if !feasible
-            # Feasibility cut
-            # We know demand <= x + y <= CAP_GEN + pCmax
-            # => pCmax >= (demand - CAP_GEN)
-            # So we add:
-            println("    Subproblem infeasible => feasibility cut: pC >= ", sc.demand - CAP_GEN)
-            @constraint(master, pC >= sc.demand - CAP_GEN)
-
+            # Feasibility cut:
+            # Here we add a cut on the total candidate capacity
+            println("  Subproblem infeasible => feasibility cut: total_pC >= ", maxD - Pgmax_total)
+            @constraint(master, total_pC >= maxD - Pgmax_total)
         else
             # Optimality cut
+            # Each candidate contributes with its partial derivative (muY[j])
             #
-            # Weighted subproblem cost = sc.weight * subcost
-            # The partial derivative w.r.t. pC is sc.weight * muY
-            #
-            # => θ[i] >= sc.weight*subcost + sc.weight*muY*(pC - pC_val)
-            #
-            println("    Subproblem feasible => cost=$subcost muY=$muY")
+            # => θ[i] >= subcost + sum(muY[j]*(pC[j] - pC_val[j]) for j in 1:n_candidates)
+            println("  Subproblem feasible => cost=", subcost, " muY=", muY)
             @constraint(master, 
-                # θ[i] >= sc.weight*subcost + sc.weight*muY*(pC - pC_val)
-                θ[i] >= subcost + muY*(pC - pC_val)
+            θ[ω] >= subcost + sum(muY[c]*(pC[c] - pC_val[c]) for c in C)
             )
         end
     end
-
+    # Check stopping criteria
+    if abs(master_obj_k - master_obj) < tolerance
+        println("Stopping criteria met: Change in master_obj = ", 
+                abs(master_obj_k - master_obj))
+        break
+    end
+    master_obj_k = master_obj    
 end
 
 optimize!(master)
 println("\nFinal solution:")
-println("  pC = ", value(pC))
-for i in eachindex(SCENARIOS)
-    println("  θ[$i] = ", value(θ[i]))
+println("  Candidate capacities (pC) = ", [value(pC[c]) for c in C])
+for ω in Ω
+    println("  θ[$ω] = ", value(θ[ω]))
 end
 println("  Obj = ", objective_value(master))
+
+nΩ = length(Ω)
+iters = length(his[:master_obj])
+
+axispC = transpose(hcat(his[:pC]...))
+colorsce = collect(cgrad(:Greens, nC, rev = true))
+colorsce = colorsce[1:nC]
+
+plot(axispC, 
+    xlabel="Iteration", 
+    ylabel="Candidate capacity [MW]", 
+    label=[L"c_1" L"c_2"],
+    lw=2,
+    st=:bar,
+    color=colorsce',
+    linecolor = :match,
+    bar_width = 0.9,
+    legend=:topleft,
+    ylims=(0,500),
+)
+
+plot!(twinx(), his[:master_obj]/1e6, 
+    xlabel="Iteration", 
+    xticks=(1:max_iters, 0:max_iters-1),
+    ylabel="Objective value \$", 
+    label="Master Obj",
+    lw=4,
+    marker = :circle,
+    markersize = 8,
+    markerstrokewidth = 0,
+    legend=:topright,
+    ylims=(0,5000),
+)
+
+savefig("GridOpt.jl/src/ev/plots/master.pdf")
+
+axisb = [1 + i * (nΩ + 1) + (j - 1) for i in 0:(iters-1), j in 1:nΩ]
+axisb = axisb'[:]
+colorsce = collect(palette(:PuBuGn, nΩ+1, rev = true))
+colorsce = colorsce[1:nΩ]
+plot(axisb, his[:subcost]/1e6, 
+    group = repeat([1, 2, 3], outer = iters),
+    xlabel = "Iteration", 
+    ylabel = "Subproblem cost [\$]", 
+    label = [L"\omega_1" L"\omega_2" L"\omega_3" "Series 4"],
+    linecolor = :match,
+    bar_width = 0.9,
+    ylims = (0, 1.2*maximum(his[:subcost])/1e6),
+    legend=:top,
+    legend_columns = nΩ,
+    st = :bar,
+    color = colorsce',
+    xticks = (2:(nΩ+1):nΩ*(iters+1), 1:5),
+)
+
+savefig("GridOpt.jl/src/ev/plots/scenarios.pdf")
--- src/ev/models.jl
+++ src/ev/models.jl
@@ -1,4 +1,10 @@
-function market_model(mpc, df, pCmax, costC)
+# mpc = parse_mpc("GridOpt.jl/data/case5_strg.m")
+# df = 1
+# pCmax = [0.0, 0.0]
+# costC = [50.0, 50.0]
+# busC = [2, 4]
+
+function market_model(mpc, df, pCmax, costC, busC)
     # ==========================================================================
     # Sets and indices
     nT = 25                          # number of time periods
@@ -16,8 +22,8 @@ function market_model(mpc, df, pCmax, costC)
     # Find the slack bus (bus with value 3 in 2nd field)
     ref = findfirst(x -> x[2] == 3, mpc["bus"])
 
-    C = [1]
-    bC = 2
+    C = collect(1:length(costC))                # number of candidates
+    bC = busC
     # ==========================================================================
     # Parameters
     Sbase = 100
@@ -42,19 +48,23 @@ function market_model(mpc, df, pCmax, costC)
     PEVdis  = [mpc["storage"][i][7] for i in E] ./ Sbase
     eta_ch  = [mpc["storage"][i][8] for i in E]
     eta_dis = [mpc["storage"][i][9] for i in E]
-    Emax = [mpc["storage"][i][5] for i in E] ./ Sbase
-    Emin = 0.2 .* Emax
+    Emax = [mpc["storage"][i][5]*df for i in E] ./ Sbase
+    Emin = [0.3, 0.2] .* Emax
     Eo   = [mpc["storage"][i][4] for i in E] ./ Sbase
 
     # Time-varying demand factor (example: sinusoidal variation)
     PD_factor = Dict(t => 0.9 - 0.3*sin(1.7*pi*(t-1)/nT) for t in T)
 
     pCmax = pCmax / Sbase
-    total_gen = sum(Pmax) + pCmax
+    total_gen = sum(Pmax) + sum(pCmax)
     total_dem = sum(values(PD))
-    if total_dem > total_gen
-        return (0.0, 0.0, false)
-    end
+    maxD = maximum(total_dem * v for v in values(PD_factor))
+    println("Subproblem data: ")
+    println("  Total generation: ", total_gen)
+    println("  Total demand: ", maxD)
+    # if total_dem > total_gen
+    #     return (0.0, 0.0, false)
+    # end
 
     # ==========================================================================    
     # Model
@@ -80,34 +90,33 @@ function market_model(mpc, df, pCmax, costC)
     )
     @constraint(m, [t in T], theta[ref, t] == 0)
     @constraint(m, [n in N, t in T], -pi <= theta[n, t] <= pi)
-    # @constraint(m, lambda[n in N, t in T], PD[n]*PD_factor[t] ==
-    #                 sum(pc[c, t] for c in C if bC[c] == n) +
-    #                 sum(pg[g, t] for g in G if bG[g] == n) - 
-    #                 sum(f[l, t] for l in L if bL[l][1] == n) + 
-    #                 sum(f[l, t] for l in L if bL[l][2] == n) +
-    #                 sum(pEV_dis[e, t] for e in E if bE[e] == n) -
-    #                 sum(pEV_ch[e, t] for e in E if bE[e] == n)
-    # )
-
-    @constraint(m, lambda[n in N, t in T], PD[n] * PD_factor[t] == 
+    @constraint(m, lambda[n in N, t in T], PD[n]*PD_factor[t] ==
                     sum(pc[c, t] for c in C if bC[c] == n) +
                     sum(pg[g, t] for g in G if bG[g] == n) - 
                     sum(f[l, t] for l in L if bL[l][1] == n) + 
-                    sum(f[l, t] for l in L if bL[l][2] == n)
+                    sum(f[l, t] for l in L if bL[l][2] == n) +
+                    sum(pEV_dis[e, t] for e in E if bE[e] == n) -
+                    sum(pEV_ch[e, t] for e in E if bE[e] == n)
     )
+
+    # @constraint(m, lambda[n in N, t in T], PD[n] * PD_factor[t] == 
+    #                 sum(pc[c, t] for c in C if bC[c] == n) +
+    #                 sum(pg[g, t] for g in G if bG[g] == n) - 
+    #                 sum(f[l, t] for l in L if bL[l][1] == n) + 
+    #                 sum(f[l, t] for l in L if bL[l][2] == n)
+    # )
 
     @constraint(m, [e in E, t in T], 0 <= pEV_ch[e, t] <= PEVch[e])
     @constraint(m, [e in E, t in T], 0 <= pEV_dis[e, t] <= PEVdis[e])
     @constraint(m, [e in E, t in T], Emin[e] <= eEV[e, t] <= Emax[e])
     @constraint(m, [e in E, t in T], pEV_ch[e, t]*pEV_dis[e, t] == 0)
     @constraint(m, [e in E], eEV[e, 1] == Eo[e])
-
-    @constraint(m, [e in E, t in T], eEV[e, t] == eEV[e, t-1] + 
+        @constraint(m, [e in E, t in T], eEV[e, t] == eEV[e, t-1] + 
                     eta_ch[e]*pEV_ch[e, t] - 
                     pEV_dis[e, t]/eta_dis[e]
     )
 
-    @constraint(m, yup[c in C, t in T], 0 <= pc[c, t] <= pCmax)
+    @constraint(m, yup[c in C, t in T], 0 <= pc[c, t] <= pCmax[c])
 
     # ==========================================================================
     # Objective
@@ -120,10 +129,124 @@ function market_model(mpc, df, pCmax, costC)
 
     # ==========================================================================
     # Print results
-    println("Objective value: ", objective_value(m))
-    println("Status: ", termination_status(m))
+    println("  Objective value: ", objective_value(m))
+    println("  Status: ", termination_status(m))
+
+    maxD = maximum(total_dem * v for v in values(PD_factor)) * Sbase
+    println("        Max demand: ", maxD)
+    totalG = [sum(value(pg[g, t]) for g in G) + sum(value(pc[c, t]) for c in C) for t in T]
+    maxG = maximum(totalG) * Sbase
+    println("        Max total gen: ", maxG)
+
+    # market_plots(pg, pc, eEV, pEV_ch, pEV_dis, total_dem, PD_factor, lambda)
 
     return (objective_value(m), 
-            maximum([dual(yup[c, t]) for c in C, t in T]), 
-            termination_status(m) == LOCALLY_SOLVED)
+            [maximum(dual(yup[c, t]) for t in T) for c in C],
+            termination_status(m) == LOCALLY_SOLVED,
+            maxD)
+            
 end
+
+function market_plots(pg, pc, eEV, pEV_ch, pEV_dis, total_dem, PD_factor, lambda)
+    # Extract the values of pg and pc
+    pg_data = [round(value(pg[g, t]), digits=3) for g in G, t in T]
+    pc_data = [round(value(pc[c, t]), digits=3) for c in C, t in T]
+
+    eEV_data = [round(value.(eEV[e,t]), digits = 3) for e in E, t in T]
+    pEV_ch_data = [round(value(pEV_ch[e,t]), digits = 3) for e in E, t in T]
+    pEV_dis_data = [round(value(pEV_dis[e,t]), digits = 3) for e in E, t in T]
+
+    color = collect(cgrad(:GnBu, nG, rev = true))
+    colorg = color[1:nG]
+    color = collect(cgrad(:Greens, nC, rev = true))
+    colorc = color[1:nC]
+
+    l = @layout [a{0.4h}; b{0.6h}]
+
+    p1 = areaplot(1:24, [pg_data' pc_data']*Sbase, 
+            ylabel="Power (MW)", 
+            xlims=(1, 24), 
+            label="",
+            color=[colorg; colorc]',
+            lw = 0
+    )
+
+    demand_h = [total_dem*PD_factor[t] for t in T]*Sbase
+    plot!(p1, 1:24, demand_h, 
+        color="#a50f15", 
+        lw = 3, 
+        la = 0.6,
+        ls = :solid,
+        label="Demand",
+        legend = :topleft
+    )
+
+    p2 = areaplot(1:24, [pg_data' pc_data']*Sbase, 
+            xlabel="Time (h)", 
+            ylabel="Power (MW)", 
+            xlims=(1, 24), 
+            label=[L"G_1" L"G_2" L"G_3" L"G_4" L"G_5" L"C_1" L"C_2"],
+            color=[colorg; colorc]',
+            lw = 0,
+            ylims=(0.95*minimum(demand_h), 1.05*maximum(demand_h)),
+            legendcolumns = 2,
+            legend = :topleft,
+    )
+
+    demand_h = [total_dem*PD_factor[t] for t in T]*Sbase
+    plot!(p2, 1:24, demand_h, 
+        color="#a50f15", 
+        lw = 2, 
+        la = 0.6,
+        ls = :solid,
+        label="",
+    )
+
+    plot(p1, p2, layout=l)
+
+    # Save the plot
+    savefig("GridOpt.jl/src/ev/plots/stacked_gen.pdf")
+
+    color = collect(cgrad(:Purples_5, nE, rev = true))
+    colore = color[1:nE]
+
+    layout = @layout [a; b]
+
+    pe = areaplot(1:24, eEV_data'*Sbase, 
+        ylabel = "Energy (MWh)",
+        color = colore',
+        xlims=(1, 24),
+        label=[L"a_3" L"a_2"], 
+    )
+    pp = areaplot(1:24, pEV_ch_data'*Sbase - pEV_dis_data'*Sbase,
+        color = colore',
+        xlims=(1, 24),
+        xlabel = "Time (h)",
+        ylabel = "Power (MW)",
+        label=[L"a_3" L"a_2"], 
+    )
+    plot(pe, pp, layout=layout)
+
+    savefig("GridOpt.jl/src/ev/plots/stacked_sto.pdf")
+
+    # Plot lambda
+    lambda_data = [round(-value(lambda[n, t]), digits=3) for n in N, t in T]
+    axisN = collect(1:length(N))
+    axisT = collect(1:length(T))
+    po = heatmap(lambda_data, levels = 5,
+        xlabel="Time (h)", 
+        ylabel="Bus", 
+        yticks=(axisN, N),
+        xticks=(1:2:24, 1:2:24),
+        color=cgrad(:RdBu, rev = true), 
+        # title=L"\lambda", 
+        clims=(minimum(lambda_data), maximum(lambda_data)),
+    )
+
+    savefig(po, "GridOpt.jl/src/ev/plots/price_heatmap.pdf")
+
+end
+
+# lambda_data
+# axisN
+# # market_model(mpc, df, pCmax, costC, busC)
